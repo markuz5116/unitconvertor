5853b1fc0e45a28ca70d5fa2cab1fb5fa6e601e7
Marcus Ong <markuz5116@gmail.com>
2025-10-05 16:04:45 +0800

    Optimize imports

--- src/main/java/com/beginner/unitconvertor/controllers/UnitConvertorController.java
+++ src/main/java/com/beginner/unitconvertor/controllers/UnitConvertorController.java
@@ -1,9 +1,6 @@
 package com.beginner.unitconvertor.controllers;
 
-import javax.management.RuntimeErrorException;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
